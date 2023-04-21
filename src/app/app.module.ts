eb7460a6043067f250bd59515f2ceee2b1705c9e
Juan Ordonez <juan.ordonez.hurtado@correounivalle.edu.co>
2023-04-20 20:12:05 -0500

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscarProductosComponent } from './buscar-productos/buscar-productos.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactarComponent } from './contactar/contactar.component';
+
+const appRoutes:Routes =
+[
+  {path: 'buscar_productos', component:BuscarProductosComponent},
+  {path: 'contactar', component:ContactarComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarProductosComponent,
+    ContactarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
